a2e344cb2674e02b9329d32cee387968b8d9ab50
aquadri <aquadri@UIT-IRA-2B35MN2.yorku.yorku.ca>
2020-05-08 11:31:17 -0400

    This is my first attempt, lets see how it goes

---
+++ FirstAttempt/src/FirstAttempt.java
@@ -0,0 +1,13 @@
+public class FirstAttempt{
+
+	public static void main(String[] args) {
+		JokeBank joke = new JokeBank();
+		
+		
+		System.out.println(joke.jokeList[1]);
+	
+	}
+
+}
+
+
---
+++ FirstAttempt/src/JokeBank.java
@@ -0,0 +1,16 @@
+
+public class JokeBank {
+	
+	String[] jokeList;
+	
+	public JokeBank() {
+		
+	jokeList = new String[5];
+	
+	//make 3 more jokes and then run a loop in the main class to run em all
+	//can add anything cool that you want to but keep it simple, our
+	// purpose is to test GitHub, not our programming abilities.
+	jokeList[0] = "Thats so punny!";
+	jokeList[1] = "These jokes are Egg-cellant";
+	}
+}
