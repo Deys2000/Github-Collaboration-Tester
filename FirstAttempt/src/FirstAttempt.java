56afee3e3ae2f79444bc8bedad03a72e1363adce
aquadri <aquadri@UIT-IRA-2B35MN2.yorku.yorku.ca>
2020-05-09 19:21:50 -0400

    JFrame added
    Made Jframe interface so JAR file could be created and run

--- FirstAttempt/src/FirstAttempt.java
+++ FirstAttempt/src/FirstAttempt.java
@@ -1,11 +1,41 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+/*
+ * This is the main class to run
+ */
+
 public class FirstAttempt{
 
 	public static void main(String[] args) {
-		JokeBank joke = new JokeBank();
-		for (int i=0;i<5; i++) {
-		System.out.println(joke.jokeList[i]);
-		}
-	
+		// making joke bank object to use later
+		JokeBank jokeArray = new JokeBank();
+
+		//setting up the window
+		JFrame frame = new JFrame();
+		frame.setSize(600, 400);
+		frame.setTitle("Random Joke Generator");
+		frame.getContentPane().setLayout(new FlowLayout());
+		//setting up the items in the window
+		JLabel label = new JLabel("*Joke will show up here*",10);
+		JButton button = new JButton("Random Joke");
+		frame.getContentPane().add(label);
+		frame.getContentPane().add(button);
+		frame.setVisible(true);
+		
+		//setting up the button functionality so that it will generate joke from bank
+		button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				String joke;
+				joke = jokeArray.jokeList[(int)(Math.random()*jokeArray.jokeList.length)];
+				label.setText(joke);
+			}        
+		});        
 	}
 
 }
