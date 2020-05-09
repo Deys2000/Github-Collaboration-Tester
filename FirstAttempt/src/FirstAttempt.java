c76d139d9d90e02ca42fa6ab0f92ac5f36dc17b0
aquadri <aquadri@UIT-IRA-2B35MN2.yorku.yorku.ca>
2020-05-09 12:44:38 -0400

    Testing if this will update the master or detailed branch

--- FirstAttempt/src/FirstAttempt.java
+++ FirstAttempt/src/FirstAttempt.java
@@ -2,7 +2,7 @@ public class FirstAttempt{
 
 	public static void main(String[] args) {
 		JokeBank joke = new JokeBank();
-		for (int i=0;i<5; i++) {
+		for (int i=0;i<4; i++) {
 		System.out.println(joke.jokeList[i]);
 		}
 	
