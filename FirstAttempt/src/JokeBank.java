39db22d5bb3d989340950797771b40619dec018a
Mohammed Ahmed Fulwala <48979024+Mommet@users.noreply.github.com>
2020-05-09 05:36:23 -0400

    Update JokeBank.java

--- FirstAttempt/src/JokeBank.java
+++ FirstAttempt/src/JokeBank.java
@@ -5,7 +5,7 @@ public class JokeBank {
 	
 	public JokeBank() {
 		
-	String jokeList = new String[5];
+	jokeList = new String[5];
 	
 	//make 3 more jokes and then run a loop in the main class to run em all
 	//can add anything cool that you want to but keep it simple, our
